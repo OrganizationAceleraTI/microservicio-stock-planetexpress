75f3b66047e9e28fa93181ac1c6813a827dd8018
Sergio Rodriguez <sergio.rodriguezra@pragma.com.co>
2023-02-09 18:49:16 -0500

    Initial commit

---
+++ src/test/java/co/acelerati/planetexpressstock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package co.acelerati.planetexpressstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

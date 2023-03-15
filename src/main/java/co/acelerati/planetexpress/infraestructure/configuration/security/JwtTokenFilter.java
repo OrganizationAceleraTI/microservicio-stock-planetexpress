ba0c5a3f884dda74f797b222413832ef8e024f1f
Esteban Bula Angulo <36938874+estebanbula@users.noreply.github.com>
2023-03-14 22:20:44 -0500

    Implementación sprint security

---
+++ src/main/java/co/acelerati/planetexpress/infraestructure/configuration/security/JwtTokenFilter.java
@@ -0,0 +1,65 @@
+package co.acelerati.planetexpress.infraestructure.configuration.security;
+
+import co.acelerati.planetexpress.infraestructure.configuration.security.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    private List<String> excludedPrefixes = Arrays.asList("/auth/**", "/swagger-ui/**");
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
+      throws ServletException, IOException {
+        try {
+            String token = getToken(req);
+            System.out.println("token::" + token);
+            if (token != null && jwtProvider.validateToken(token) && jwtProvider.getNombreUsuarioFromToken(token) != null) {
+                UserDetails userDetails = getUserDetails(token);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                  userDetails, null, userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("doFilter error: " + e);
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+        }
+        filterChain.doFilter(req, res);
+    }
+
+    private UserDetails getUserDetails(String token) {
+        return new User(0L, jwtProvider.getNombreUsuarioFromToken(token), jwtProvider.getRole(token));
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null) {
+            return header.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
